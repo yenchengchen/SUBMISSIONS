39bdd16cf835d0ad3fe97eb656d0cca933690c20
yenchengchen <88852385+yenchengchen@users.noreply.github.com>
2021-09-05 23:58:36 -0700

    Add files via upload

---
+++ week_02/palindrome.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 23:04:16 2021
+
+@author: Yencheng
+"""
+
+name = str(input("your name?",))
+
+i = 0
+newname = ""
+newname = newname + name[len(name)-1].upper()
+
+while i <  len(name)-1:
+    
+    
+    newname = newname + name[len(name)-i-2].lower() 
+    
+    i += 1
+    
+# palindrome check
+
+namefor = newname
+namefor = namefor + name[0].lower()
+
+i = 1
+while i < len(name)-1:
+    
+    namefor = namefor + name[i].lower()
+    
+    i += 1
+    
+    
+namerev = ""
+namerev = name
+namerev = namerev + name[0].lower()
+
+i = 1
+while i < len(name)-1:
+    
+    namefor = namefor + name[i].lower()
+    
+    i += 1
+
+if newfor == namerev:
+    print("Palindrome!")
+
+
+else:     
+    
+    print(newname)
